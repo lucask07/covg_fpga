dbcc388b09313a81d4b2b1b000ee1281f8c2be90
delg5279 <73324577+delg5279@users.noreply.github.com>
2021-01-06 17:42:34 -0600

    Update WbSignal_converter.v

--- Verilog/Control_StateMachine/WbSignal_converter.v
+++ Verilog/Control_StateMachine/WbSignal_converter.v
@@ -11,19 +11,6 @@
 // Tool Versions: 
 // Description: 
 // 
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: UST
-// Engineer: Ian Delgadillo Bonequi
-// 
-// Create Date: 11/30/2020 10:01:41 PM
-// Design Name: 
-// Module Name: WbSignal_converter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
 // Dependencies: 
 // 
 // Revision:
@@ -40,8 +27,8 @@ module WbSignal_converter(clk, rst, ep_dataout, trigger, o_stb, cmd_word, int_o/
     input wire [31:0] ep_dataout;
     input wire trigger;
     input wire int_o;
-	 /*input wire [31:0] tx_spi_dat;//this signal and the one below will be used to detect manual/one-shot read requests
-	 input wire [13:0] control_dat;//*/
+    /*input wire [31:0] tx_spi_dat;//this signal and the one below will be used to detect manual/one-shot read requests
+    input wire [13:0] control_dat;//*/
     output reg o_stb;
     output reg [33:0] cmd_word;
     
@@ -52,6 +39,7 @@ module WbSignal_converter(clk, rst, ep_dataout, trigger, o_stb, cmd_word, int_o/
     parameter S1 = 5'b00001;
     parameter S2 = 5'b00010;
     parameter S3 = 5'b00011;
+    parameter S4 = 5'b01111;
     parameter read = 5'b00100;//this state and the ones below will initiate transfer of data from the ADC to the FIFO
     parameter read1 = 5'b00101;//this will ensure that there is not need for host intervention to move data from the SPI master to the FIFO
     parameter read2 = 5'b00110;
@@ -79,20 +67,23 @@ module WbSignal_converter(clk, rst, ep_dataout, trigger, o_stb, cmd_word, int_o/
                 if(trigger)begin
                     nextstate = S1;
                 end
-		else if(int_o)begin
+	        else if(int_o)begin
 		    nextstate = read;
-		end
+	        end
                 else begin
                     nextstate = S0;
                 end
             end
-            S1: begin
+	    S1: begin
                 nextstate = S2;
             end
             S2: begin
                 nextstate = S3;
             end
             S3: begin
+                nextstate = S4;
+            end
+            S4: begin
                 nextstate = S0;
             end
             read: begin
@@ -130,15 +121,19 @@ module WbSignal_converter(clk, rst, ep_dataout, trigger, o_stb, cmd_word, int_o/
                     cmd_word = {ep_dataout[31:30], 2'b0, ep_dataout[29:0]};
                     o_stb = 1'b0;
                 end
-            S1: begin
+				S1: begin
                     cmd_word = cmd_word;
-                    o_stb = 1'b1;
+                    o_stb = 1'b0;
             end
             S2: begin
                     cmd_word = cmd_word;
                     o_stb = 1'b1;
             end
             S3: begin
+                    cmd_word = cmd_word;
+                    o_stb = 1'b1;
+            end
+            S4: begin
                     cmd_word = cmd_word;
                     o_stb = 1'b0;
             end
