6194c2b7beb273c62f7e2c4bee7b70415c2893d3
Ajstros <ajstros@gmail.com>
2021-07-12 16:11:16 -0500

    Fixed DAC python errors before FPGA connection

--- python/tests/DAC80508_test.py
+++ python/tests/DAC80508_test.py
@@ -1,15 +1,20 @@
 # Test the configuration, read, and most importantly write functionality of the DAC
 import logging
 import sys, os
-cwd = os.getcwd()
-interfaces_path = os.path.join(cwd, '..')
-registers_path = os.path.join(cwd, '..', '..')
+
+# The interfaces.py file is located in the covg_fpga folder so we need to find that folder. If it is not above the current directory, the program fails.
+interfaces_path = os.getcwd()
+for i in range(15):
+    if os.path.basename(interfaces_path) == 'covg_fpga':
+        interfaces_path = os.path.join(interfaces_path, 'python')
+        break
+    else:
+        interfaces_path = os.path.dirname(interfaces_path)  # If we aren't in covg_fpga, move up a folder and check again
 sys.path.append(interfaces_path)
-sys.path.append(registers_path)
 
 from interfaces import FPGA, GeneralDACController
 
-logging.basicConfig(filename='DAC80508_test.log',
+logging.basicConfig(filename='DAC80508_test.log', filemode='w',
                     encoding='utf-8', level=logging.INFO)
 
 # Set up FPGA
@@ -18,21 +23,32 @@ f.init_device()
 
 # Set up DAC
 gp_dac = GeneralDACController(f)
+
 id = gp_dac.get_id(display=True)
-logging.info(f'ID={id}')
+if id == False:
+    logging.warning(f'ID={id}')
+else:
+    logging.info(f'ID={id}')
+
 reset_success = gp_dac.reset()
 if reset_success:
-    logging.info('Reset succesful')
+    logging.info('Reset: SUCCESS')
 else:
-    logging.warning('Reset failed')
+    logging.warning('Reset: FAIL')
 
 # Read configuration register
-configuration = gp_dac.get_config(display_values=True)
-logging.info(f'Configuration={configuration}')
+config = gp_dac.get_config(display_values=True)
+if config == False:
+    logging.warning(f'Configuration={config}')
+else:
+    logging.info(f'Configuration={config}')
 
 # Write to dac0
-for voltage in range(0x0005):
+for voltage in range(0x0006):
     if gp_dac.write('DAC0', voltage):
-        logging.info('Write {hex(voltage)}={voltage}V Successful')
+        message = f'Write {hex(voltage)}={voltage}V SUCCESS'
+        print(message)
+        logging.info(message)
     else:
-        logging.info('Write {hex(voltage)}={voltage}V Failed')
+        message = f'Write {hex(voltage)}={voltage}V FAIL'
+        logging.warning(message)
