e2a023e550db6e11cb2527f3fe350efec839cf7b
Lucas Koerner <koerner.lucas@gmail.com>
2021-11-11 15:51:22 -0600

    changed ddr cmd to be before app_en

--- fpga_XEM7310/fpga_XEM7310.srcs/sim_1/new/tb_dual_port_ddr.v
+++ fpga_XEM7310/fpga_XEM7310.srcs/sim_1/new/tb_dual_port_ddr.v
@@ -33,7 +33,6 @@ reg ddr_reset;
      ////////////////////////////*DDR3 INTERFACE INSTANTIATIONS*//////////////////////////////////////////////
      // OK RAMTest Parameters
 
- /*
      localparam BLOCK_SIZE = 512; // 512 bytes / 4 bytes per word. LJK update from 128 to 512. 
      localparam FIFO_SIZE = 1023; // note that Xilinx does not allow use of the full 1024 words
      localparam BUFFER_HEADROOM = 20; // headroom for the FIFO count to account for latency
@@ -42,53 +41,88 @@ reg ddr_reset;
      // [0] - Supports passing calibration status through FrontPanel
      localparam CAPABILITY = 16'h0001;
 
-     wire          init_calib_complete;
+     reg          init_calib_complete;
      reg           rst_sys_ddr; // reset at startup for a certain number of clock cycles 
 
      wire [29 :0]  app_addr;
      wire [2  :0]  app_cmd;
      wire          app_en;
-     wire          app_rdy;
-     wire [255:0]  app_rd_data;
+     
+//     wire          app_rdy;
+     reg          app_rdy;
+     
+     //wire [255:0]  app_rd_data;
+     reg [255:0]  app_rd_data;
+
      wire          app_rd_data_end;
-     wire          app_rd_data_valid;
+     
+     //wire          app_rd_data_valid;
+     reg          app_rd_data_valid;
+
      wire [255:0]  app_wdf_data;
      wire          app_wdf_end;
      wire [31 :0]  app_wdf_mask;
-     wire          app_wdf_rdy;
+     
+//     wire          app_wdf_rdy;
+     reg          app_wdf_rdy;
+
      wire          app_wdf_wren;
 
-     wire          clk_ddr_ui;
+     //wire          clk_ddr_ui;
      wire          rst_ddr_ui;
+     //wire [31:0]  ep03wire;
 
-     wire [31:0]  ep03wire;
-
-
-
-     
      wire          ddr3_rst;// MIG/DDR3 synchronous reset
 
+     reg ep_ddr_reset;
+
+     wire         pipe_in_read;
+     wire [255:0] pipe_in_data;
+     wire [6:0]   pipe_in_rd_count;
+     wire [9:0]   pipe_in_wr_count;
+     wire         pipe_in_valid;
+     wire         pipe_in_full;
+     wire         pipe_in_empty;
+     reg          pipe_in_ready;
+
+     wire         pipe_out_write;
+     wire [255:0] pipe_out_data;
+     wire [7:0]   pipe_out_rd_count; //LJK to change
+     wire [6:0]   pipe_out_wr_count;
+     wire         pipe_out_full;
+     wire         pipe_out_empty;
+     reg          pipe_out_ready;
+
+     wire         pipe_in2_read;
+     wire [255:0] pipe_in2_data;
+     wire [6:0]   pipe_in2_rd_count;
+     wire [8:0]   pipe_in2_wr_count;  //LJK to change 
+     wire         pipe_in2_valid;
+     wire         pipe_in2_full;
+     wire         pipe_in2_empty;
+     reg          pipe_in2_ready;
+
+     wire         pipe_out2_write;
+     wire [255:0] pipe_out2_data;
+     wire [9:0]   pipe_out2_rd_count;
+     wire [6:0]   pipe_out2_wr_count;
+     wire         pipe_out2_full;
+     wire         pipe_out2_empty;
+     reg          pipe_out2_ready;
+
+     wire [127:0]  po0_ep_datain;// MIG/DDR3 FIFO data out to DACs 
+     
+     //wire         po2_ep_read;
+     reg         po2_ep_read;
+     wire [31:0]  po2_ep_datain;// MIG/DDR3 ADC FIFO data out
+     wire [15:0] adc_data_cnt; // count of ADC words in DDR
+     
+     // Pipe Fifos
+     //wire         pi0_ep_write;
+     wire         po0_ep_read;
+     wire [31:0]  pi0_ep_dataout;
 
 /*
-     reset_synchronizer u_MIG_sync_rst( //TODO: move this to a trigger in
-     .clk(clk_sys),
-     .async_rst(ep03wire[`DDR3_RESET]),
-     .sync_rst(ddr3_rst)
-     );
-
-     //MIG Infrastructure Reset
-     reg [31:0] rst_cnt;
-     initial rst_cnt = 32'b0;
-     always @(posedge okClk) begin
-         if(rst_cnt < 32'h0800_0000) begin
-             rst_cnt <= rst_cnt + 1;
-             rst_sys_ddr <= 1'b1;
-         end
-         else begin
-             rst_sys_ddr <= 1'b0;
-         end
-     end
-
      // MIG User Interface instantiation
      ddr3_256_32 u_ddr3_256_32 (
          // Memory interface ports
@@ -134,6 +168,35 @@ reg ddr_reset;
          .sys_clk_i                      (clk_sys),
          .sys_rst                        (rst_sys_ddr)
          );
+*/
+
+reg okClk;
+    
+ reset_synchronizer u_MIG_sync_rst( //TODO: move this to a trigger in
+    .clk(clk_sys),
+    .async_rst(ep_ddr_reset),
+    .sync_rst(ddr3_rst)
+);
+
+//MIG Infrastructure Reset
+reg [31:0] rst_cnt;
+initial rst_cnt = 32'b0;
+always @(posedge okClk) begin
+    if(rst_cnt < 32'h0800_0000) begin
+        rst_cnt <= rst_cnt + 1;
+        rst_sys_ddr <= 1'b1;
+    end
+    else begin
+        rst_sys_ddr <= 1'b0;
+    end
+end
+    reg [31:0] INDEX;
+    reg [31:0] INDEX2;
+            
+    reg ep_read_enable;
+    reg ep_write_enable;
+    reg ep_fgread_enable;
+    
 
      // OK MIG DDR3 Testbench Instatiation
      ddr3_test ddr3_tb (
@@ -142,9 +205,9 @@ reg ddr_reset;
          .INDEX2              (INDEX2),
 
          .reset              (ddr3_rst | rst_ddr_ui),
-         .reads_en           (ep03wire[`DDR3_READ_ENABLE]),
-         .writes_en          (ep03wire[`DDR3_WRITE_ENABLE]),
-         .fg_reads_en        (ep03wire[`DDR3_FG_READ_ENABLE]),
+         .reads_en           (ep_read_enable),
+         .writes_en          (ep_write_enable),
+         .fg_reads_en        (ep_fgread_enable),
          .calib_done         (init_calib_complete),
 
          .ib_re              (pipe_in_read),
@@ -171,70 +234,43 @@ reg ddr_reset;
          
          .adc_data_count        (adc_data_cnt),
 
-         .app_rdy            (app_rdy),
-         .app_en             (app_en),
-         .app_cmd            (app_cmd),
-         .app_addr           (app_addr),
+         .app_rdy            (app_rdy), //input 
+         .app_en             (app_en), // output 
+         .app_cmd            (app_cmd), // output [2:0]
+         .app_addr           (app_addr), // output [29:0]
 
-         .app_rd_data        (app_rd_data),
-         .app_rd_data_end    (app_rd_data_end),
-         .app_rd_data_valid  (app_rd_data_valid),
+         .app_rd_data        (app_rd_data), //input  [255:0]
+         .app_rd_data_end    (1'b0), //input (NOT USED)
+         .app_rd_data_valid  (app_rd_data_valid), //input 
 
-         .app_wdf_rdy        (app_wdf_rdy),
-         .app_wdf_wren       (app_wdf_wren),
-         .app_wdf_data       (app_wdf_data),
-         .app_wdf_end        (app_wdf_end),
-         .app_wdf_mask       (app_wdf_mask)
+         .app_wdf_rdy        (app_wdf_rdy), //input 
+         .app_wdf_wren       (app_wdf_wren), //output 
+         .app_wdf_data       (app_wdf_data), //output [255:0]
+         .app_wdf_end        (app_wdf_end), //output 
+         .app_wdf_mask       (app_wdf_mask) //output [31:0]
          );
-*/
-
-     //wire         pipe_in_read;
-     //wire [255:0] pipe_in_data;
-     //wire [6:0]   pipe_in_rd_count;
-     //wire [9:0]   pipe_in_wr_count;
-     //wire         pipe_in_valid;
-     //wire         pipe_in_full;
-     //wire         pipe_in_empty;
-     //reg          pipe_in_ready;
-
-     //wire         pipe_out_write;
-     //wire [255:0] pipe_out_data;
-     wire [8:0]   pipe_out_rd_count; //LJK to change
-     wire [6:0]   pipe_out_wr_count;
-     wire         pipe_out_full;
-     wire         pipe_out_empty;
-     reg          pipe_out_ready;
-
-     //wire         pipe_in2_read;
-     //wire [255:0] pipe_in2_data;
-     wire [6:0]   pipe_in2_rd_count;
-     wire [8:0]   pipe_in2_wr_count;  //LJK to change 
-     //wire         pipe_in2_valid;
-     //wire         pipe_in2_full;
-     //wire         pipe_in2_empty;
-     //reg          pipe_in2_ready;
-
-     //wire         pipe_out2_write;
-     //wire [255:0] pipe_out2_data;
-     //wire [9:0]   pipe_out2_rd_count;
-     //wire [6:0]   pipe_out2_wr_count;
-     //wire         pipe_out2_full;
-     //wire         pipe_out2_empty;
-     //reg          pipe_out2_ready;
-
-     wire [127:0]  po0_ep_datain;// MIG/DDR3 FIFO data out to DACs 
      
-     //wire         po2_ep_read;
-     //wire [31:0]  po2_ep_datain;// MIG/DDR3 ADC FIFO data out
-     //wire [15:0] adc_data_cnt; // count of ADC words in DDR
-     
-     // Pipe Fifos
-     //wire         pi0_ep_write;
-     //wire         po0_ep_read;
-     //wire [31:0]  pi0_ep_dataout;
-     //wire [31:0] INDEX;
-     //wire [31:0] INDEX2;
+     assign pi0_ep_dataout = 32'ha5a5a5a5;
+     wire pi0_ep_write;
+     assign pi0_ep_write = 1'b1;
 
+    // in bound 1
+     fifo_w32_1024_r256_128 fg_pipein_fifo (  // Function generator data from the host via pi0 
+         .rst(ddr3_rst),
+         .wr_clk(okClk),
+         .rd_clk(clk_ddr_ui),
+         .din(pi0_ep_dataout), // Bus [31 : 0]
+         .wr_en(pi0_ep_write),
+         .rd_en(pipe_in_read),
+         .dout(pipe_in_data), // Bus [255 : 0] -- to DDR 
+         .full(pipe_in_full),
+         .empty(pipe_in_empty),
+         .valid(pipe_in_valid),
+         .rd_data_count(pipe_in_rd_count), // Bus [6 : 0]  //128 available for reading (by DDR)
+         .wr_data_count(pipe_in_wr_count)); // Bus [9 : 0] //1024 available for writing 
+
+     /*---------------- ADC DDR data debug  -------------------*/
+     
      reg[7:0] five_msps_adc_pulse = 8'd0;
      reg adc_emulator_valid;
      always @(posedge clk_ddr_ui) begin
@@ -248,20 +284,37 @@ reg ddr_reset;
         end
     end
 
-reg [255:0]dac_ddr_data;
+    wire dac_data_valid; 
+
+    // DDR: read from DDR and 1) output data to DACs and 2) output data to host through BTPipeOut
+     // outbound 2
+     fifo_w256_128_r32_1024 okPipeOut_fifo_ddr2 (  // Output ADC data from DDR -> input data of ADCs to PipeOut
+         .rst(ddr3_rst), // supports asynchronous reset 
+         .wr_clk(clk_ddr_ui),
+         .rd_clk(okClk),
+         .din(pipe_out2_data), // Bus [255 : 0] -- from DDR 
+         .wr_en(pipe_out2_write),
+         .rd_en(po2_ep_read),
+         .dout(po2_ep_datain), // Bus [31 : 0]
+         .full(pipe_out2_full),
+         .empty(pipe_out2_empty),
+         .valid(),  //output
+         .rd_data_count(pipe_out2_rd_count), // Bus [9 : 0]
+         .wr_data_count(pipe_out2_wr_count)); // Bus [6 : 0]
+
+
 wire ad5453_clk_en;
 assign ad5453_clk_en = adc_emulator_valid;
 
+    // in bound 2 
      fifo_w64_512_r256_128_1 adc_ddr_fifo (  //ADC data input, output to DDR
          .rst(ddr_reset),
-         //.wr_clk(adc_timing_clk),
          .wr_clk(clk_sys),
          .rd_clk(clk_ddr_ui),
          //.din(adc_ddr_debug_cnt), // Bus [63 : 0]
          .din(po0_ep_datain[63:0]),
-         //.wr_en(write_en_adc_o[0]),
-         .wr_en(ad5453_clk_en),
-         .rd_en(1'b1),
+         .wr_en(dac_data_valid),  //LJK: changed!
+         .rd_en(pipe_in2_read), //  
          .dout(pipe_in2_data), // Bus [255 : 0] - to DDR 
          .full(pipe_in2_full),
          .empty(pipe_in2_empty),
@@ -271,46 +324,87 @@ assign ad5453_clk_en = adc_emulator_valid;
 
     // DDR: read from DDR and 1) output data to DACs and 2) output data to host through BTPipeOut
 
+    // outbound 1
      fifo_w256_128_r128_256_1 okPipeOut_fifo_ddr ( //Data in from DDR -> Output to DACs 
          .rst(ddr_reset), // supports asynchronous reset 
          .wr_clk(clk_ddr_ui),
          .rd_clk(clk_sys),
-         .din(dac_ddr_data), // Bus [255 : 0] -- from DDR 
-         .wr_en(1'b1),
-         .rd_en(ad5453_clk_en),
+         .din(pipe_out_data), // Bus [255 : 0] -- from DDR 
+         .wr_en(pipe_out_write),
+         .rd_en(ad5453_clk_en & ep_read_enable),
          .dout(po0_ep_datain), // Bus [127 : 0]
          .full(pipe_out_full),
          .empty(pipe_out_empty),
          .valid(dac_data_valid), //output 
-         .rd_data_count(pipe_out_rd_count), // Bus [9 : 0]
+         .rd_data_count(pipe_out_rd_count), // Bus [8 : 0]
          .wr_data_count(pipe_out_wr_count)); // Bus [6 : 0]
  
+
+// emulate MIG behavior 
 always @(posedge clk_ddr_ui) begin
-    if(ddr_reset == 1'b1) dac_ddr_data <=0;
-    else dac_ddr_data <= dac_ddr_data + 1'b1;
+    if ((app_cmd == 3'b001) & (app_en == 1'b1)) app_rd_data_valid <= 1'b1; //app_cmd = 001 is a read from DDR
+    else app_rd_data_valid <= 1'b0;
+end
+
+always @(posedge clk_ddr_ui) begin
+    app_wdf_rdy <= 1'b1;
+    app_rdy <= 1'b1;
+end
+
+always @(posedge clk_ddr_ui) begin
+    if ((app_cmd == 3'b001) & (app_en == 1'b1)) app_rd_data_valid <= 1'b1;
+    else app_rd_data_valid <= 1'b0;
+end
+
+always @(posedge clk_ddr_ui) begin
+    if(ddr_reset == 1'b1) app_rd_data <= 0;
+    else if ((app_cmd == 3'b001) & (app_en == 1'b1))  app_rd_data <= app_rd_data + 1'b1;
 end
  
 // Generate clock
- initial 
+initial 
     clk_sys = 1'b0;
- always 
+always 
     #5 clk_sys = ~clk_sys; //now 200 MHz sys clock
 
- initial 
+initial 
     clk_ddr_ui = 1'b0;
  always 
     #5 clk_ddr_ui = ~clk_ddr_ui; //now 200 MHz sys clock
  
+initial 
+    okClk = 1'b0;
+ always 
+    #10.2 okClk = ~okClk;
+    
 initial begin
     // Initialize Inputs
     #5;
     ddr_reset = 1'b0;
+    ep_ddr_reset = 1'b0;
+    po2_ep_read = 1'b0;
+     
+    ep_read_enable = 1'b0;
+    ep_write_enable = 1'b1;  //write into DDR 
+    ep_fgread_enable = 1'b0;
+    
+    INDEX = 32'd4096;
+    INDEX2 = 32'd8192;
+    
+    init_calib_complete = 1'b1;
+    
     #200;
     ddr_reset = 1'b1;
+    ep_ddr_reset = 1'b1;
     #200;
     ddr_reset = 1'b0;
+    ep_ddr_reset = 1'b0;
     #20000;
+    ep_read_enable = 1'b1;  
+    ep_write_enable = 1'b0;
+    #100000;
+    po2_ep_read = 1'b1;
+    #10000;
 end
- 
  
 endmodule
