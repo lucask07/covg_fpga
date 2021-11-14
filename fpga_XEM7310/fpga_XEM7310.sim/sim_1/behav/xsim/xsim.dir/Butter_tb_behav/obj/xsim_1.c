e0e1eb11c322b4fc8644cf198e4e1058bb6ed0a7
delg5279 <delg5279@stthomas.edu>
2021-11-14 12:49:09 -0600

    added file simulating ADC filter w/ sine wave and step function inputs

---
+++ fpga_XEM7310/fpga_XEM7310.sim/sim_1/behav/xsim/xsim.dir/Butter_tb_behav/obj/xsim_1.c
@@ -0,0 +1,228 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+typedef void (*funcp)(char *, char *);
+extern int main(int, char**);
+extern void execute_9(char*, char *);
+extern void execute_32(char*, char *);
+extern void execute_33(char*, char *);
+extern void execute_34(char*, char *);
+extern void execute_35(char*, char *);
+extern void execute_36(char*, char *);
+extern void execute_38(char*, char *);
+extern void execute_39(char*, char *);
+extern void execute_40(char*, char *);
+extern void execute_41(char*, char *);
+extern void execute_42(char*, char *);
+extern void execute_43(char*, char *);
+extern void execute_44(char*, char *);
+extern void execute_45(char*, char *);
+extern void execute_46(char*, char *);
+extern void execute_47(char*, char *);
+extern void execute_48(char*, char *);
+extern void execute_49(char*, char *);
+extern void execute_51(char*, char *);
+extern void execute_52(char*, char *);
+extern void execute_53(char*, char *);
+extern void execute_54(char*, char *);
+extern void vlog_simple_process_execute_0_fast_no_reg_no_agg(char*, char*, char*);
+extern void execute_254(char*, char *);
+extern void execute_256(char*, char *);
+extern void execute_258(char*, char *);
+extern void execute_260(char*, char *);
+extern void execute_262(char*, char *);
+extern void execute_263(char*, char *);
+extern void execute_264(char*, char *);
+extern void execute_265(char*, char *);
+extern void execute_267(char*, char *);
+extern void execute_270(char*, char *);
+extern void execute_271(char*, char *);
+extern void execute_272(char*, char *);
+extern void execute_273(char*, char *);
+extern void execute_274(char*, char *);
+extern void execute_275(char*, char *);
+extern void execute_276(char*, char *);
+extern void execute_277(char*, char *);
+extern void execute_169(char*, char *);
+extern void execute_170(char*, char *);
+extern void execute_171(char*, char *);
+extern void execute_172(char*, char *);
+extern void execute_12(char*, char *);
+extern void execute_14(char*, char *);
+extern void execute_16(char*, char *);
+extern void execute_18(char*, char *);
+extern void execute_20(char*, char *);
+extern void execute_22(char*, char *);
+extern void execute_24(char*, char *);
+extern void execute_26(char*, char *);
+extern void execute_28(char*, char *);
+extern void execute_30(char*, char *);
+extern void execute_173(char*, char *);
+extern void execute_174(char*, char *);
+extern void execute_175(char*, char *);
+extern void execute_176(char*, char *);
+extern void execute_177(char*, char *);
+extern void execute_178(char*, char *);
+extern void execute_179(char*, char *);
+extern void execute_180(char*, char *);
+extern void execute_181(char*, char *);
+extern void execute_182(char*, char *);
+extern void execute_183(char*, char *);
+extern void execute_184(char*, char *);
+extern void execute_185(char*, char *);
+extern void execute_186(char*, char *);
+extern void execute_188(char*, char *);
+extern void execute_189(char*, char *);
+extern void execute_190(char*, char *);
+extern void execute_191(char*, char *);
+extern void execute_192(char*, char *);
+extern void execute_193(char*, char *);
+extern void execute_194(char*, char *);
+extern void execute_195(char*, char *);
+extern void execute_196(char*, char *);
+extern void vlog_simple_process_execute_1_fast_no_reg_no_agg(char*, char*, char*);
+extern void execute_198(char*, char *);
+extern void execute_199(char*, char *);
+extern void execute_201(char*, char *);
+extern void execute_202(char*, char *);
+extern void execute_203(char*, char *);
+extern void execute_204(char*, char *);
+extern void execute_206(char*, char *);
+extern void execute_207(char*, char *);
+extern void execute_208(char*, char *);
+extern void execute_209(char*, char *);
+extern void execute_210(char*, char *);
+extern void execute_211(char*, char *);
+extern void execute_212(char*, char *);
+extern void execute_213(char*, char *);
+extern void execute_214(char*, char *);
+extern void execute_215(char*, char *);
+extern void execute_216(char*, char *);
+extern void execute_217(char*, char *);
+extern void execute_218(char*, char *);
+extern void execute_219(char*, char *);
+extern void execute_220(char*, char *);
+extern void execute_221(char*, char *);
+extern void execute_222(char*, char *);
+extern void execute_223(char*, char *);
+extern void execute_225(char*, char *);
+extern void execute_226(char*, char *);
+extern void execute_227(char*, char *);
+extern void execute_228(char*, char *);
+extern void execute_229(char*, char *);
+extern void execute_230(char*, char *);
+extern void execute_231(char*, char *);
+extern void execute_232(char*, char *);
+extern void execute_233(char*, char *);
+extern void execute_235(char*, char *);
+extern void execute_236(char*, char *);
+extern void execute_238(char*, char *);
+extern void execute_239(char*, char *);
+extern void execute_240(char*, char *);
+extern void execute_241(char*, char *);
+extern void execute_243(char*, char *);
+extern void execute_244(char*, char *);
+extern void execute_245(char*, char *);
+extern void execute_246(char*, char *);
+extern void execute_247(char*, char *);
+extern void execute_248(char*, char *);
+extern void execute_249(char*, char *);
+extern void execute_250(char*, char *);
+extern void execute_166(char*, char *);
+extern void execute_167(char*, char *);
+extern void execute_168(char*, char *);
+extern void execute_278(char*, char *);
+extern void execute_279(char*, char *);
+extern void execute_280(char*, char *);
+extern void execute_281(char*, char *);
+extern void execute_282(char*, char *);
+extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
+funcp funcTab[134] = {(funcp)execute_9, (funcp)execute_32, (funcp)execute_33, (funcp)execute_34, (funcp)execute_35, (funcp)execute_36, (funcp)execute_38, (funcp)execute_39, (funcp)execute_40, (funcp)execute_41, (funcp)execute_42, (funcp)execute_43, (funcp)execute_44, (funcp)execute_45, (funcp)execute_46, (funcp)execute_47, (funcp)execute_48, (funcp)execute_49, (funcp)execute_51, (funcp)execute_52, (funcp)execute_53, (funcp)execute_54, (funcp)vlog_simple_process_execute_0_fast_no_reg_no_agg, (funcp)execute_254, (funcp)execute_256, (funcp)execute_258, (funcp)execute_260, (funcp)execute_262, (funcp)execute_263, (funcp)execute_264, (funcp)execute_265, (funcp)execute_267, (funcp)execute_270, (funcp)execute_271, (funcp)execute_272, (funcp)execute_273, (funcp)execute_274, (funcp)execute_275, (funcp)execute_276, (funcp)execute_277, (funcp)execute_169, (funcp)execute_170, (funcp)execute_171, (funcp)execute_172, (funcp)execute_12, (funcp)execute_14, (funcp)execute_16, (funcp)execute_18, (funcp)execute_20, (funcp)execute_22, (funcp)execute_24, (funcp)execute_26, (funcp)execute_28, (funcp)execute_30, (funcp)execute_173, (funcp)execute_174, (funcp)execute_175, (funcp)execute_176, (funcp)execute_177, (funcp)execute_178, (funcp)execute_179, (funcp)execute_180, (funcp)execute_181, (funcp)execute_182, (funcp)execute_183, (funcp)execute_184, (funcp)execute_185, (funcp)execute_186, (funcp)execute_188, (funcp)execute_189, (funcp)execute_190, (funcp)execute_191, (funcp)execute_192, (funcp)execute_193, (funcp)execute_194, (funcp)execute_195, (funcp)execute_196, (funcp)vlog_simple_process_execute_1_fast_no_reg_no_agg, (funcp)execute_198, (funcp)execute_199, (funcp)execute_201, (funcp)execute_202, (funcp)execute_203, (funcp)execute_204, (funcp)execute_206, (funcp)execute_207, (funcp)execute_208, (funcp)execute_209, (funcp)execute_210, (funcp)execute_211, (funcp)execute_212, (funcp)execute_213, (funcp)execute_214, (funcp)execute_215, (funcp)execute_216, (funcp)execute_217, (funcp)execute_218, (funcp)execute_219, (funcp)execute_220, (funcp)execute_221, (funcp)execute_222, (funcp)execute_223, (funcp)execute_225, (funcp)execute_226, (funcp)execute_227, (funcp)execute_228, (funcp)execute_229, (funcp)execute_230, (funcp)execute_231, (funcp)execute_232, (funcp)execute_233, (funcp)execute_235, (funcp)execute_236, (funcp)execute_238, (funcp)execute_239, (funcp)execute_240, (funcp)execute_241, (funcp)execute_243, (funcp)execute_244, (funcp)execute_245, (funcp)execute_246, (funcp)execute_247, (funcp)execute_248, (funcp)execute_249, (funcp)execute_250, (funcp)execute_166, (funcp)execute_167, (funcp)execute_168, (funcp)execute_278, (funcp)execute_279, (funcp)execute_280, (funcp)execute_281, (funcp)execute_282, (funcp)vlog_transfunc_eventcallback};
+const int NumRelocateId= 134;
+
+void relocate(char *dp)
+{
+	iki_relocate(dp, "xsim.dir/Butter_tb_behav/xsim.reloc",  (void **)funcTab, 134);
+
+	/*Populate the transaction function pointer field in the whole net structure */
+}
+
+void sensitize(char *dp)
+{
+	iki_sensitize(dp, "xsim.dir/Butter_tb_behav/xsim.reloc");
+}
+
+void simulate(char *dp)
+{
+	iki_schedule_processes_at_time_zero(dp, "xsim.dir/Butter_tb_behav/xsim.reloc");
+	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
+	iki_execute_processes();
+
+	// Schedule resolution functions for the multiply driven Verilog nets that have strength
+	// Schedule transaction functions for the singly driven Verilog nets that have strength
+
+}
+#include "iki_bridge.h"
+void relocate(char *);
+
+void sensitize(char *);
+
+void simulate(char *);
+
+extern SYSTEMCLIB_IMP_DLLSPEC void local_register_implicit_channel(int, char*);
+extern void implicit_HDL_SCinstatiate();
+
+extern SYSTEMCLIB_IMP_DLLSPEC int xsim_argc_copy ;
+extern SYSTEMCLIB_IMP_DLLSPEC char** xsim_argv_copy ;
+
+int main(int argc, char **argv)
+{
+    iki_heap_initialize("ms", "isimmm", 0, 2147483648) ;
+    iki_set_sv_type_file_path_name("xsim.dir/Butter_tb_behav/xsim.svtype");
+    iki_set_crvs_dump_file_path_name("xsim.dir/Butter_tb_behav/xsim.crvsdump");
+    void* design_handle = iki_create_design("xsim.dir/Butter_tb_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, 0, isimBridge_getWdbWriter(), 0, argc, argv);
+     iki_set_rc_trial_count(100);
+    (void) design_handle;
+    return iki_simulate_design();
+}
