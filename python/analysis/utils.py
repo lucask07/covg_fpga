1449d05718d34de3812e1fdaee18bedd0350ee91
Koerner <koerner.lucas@gmail.com>
2022-01-19 15:08:02 -0600

    updates to process clamp data

---
+++ python/analysis/utils.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+def find_nearest(array, target):
+    """ find value in an array nearest to a target value
+
+    Arguments
+    ---------
+    array (numpy.ndarray): array of values
+    target (float): target value
+
+    Returns
+    -------
+    (float) value in the array that is closest to target
+    """
+
+    array = np.asarray(array)
+    idx = (np.abs(array - target)).argmin()
+    return array[idx]
+
+
+def find_closest_row(df, target_dict, prefilter = ('peak_area', 7e5)):
+
+    if prefilter is not None:
+        idx = df[prefilter[0]] > prefilter[1]
+        print(f'Length of index = {np.sum(idx)}')
+    else:
+        idx = [True] * len(df.index)
+
+    for k in target_dict:
+        unq = np.unique(df[idx][k])
+        c = find_nearest(unq, target_dict[k])
+        idx = idx & (output[k] == c)
+        print(f'Length of index = {np.sum(idx)}')
+    return idx
