51ea01a575b96b18c1bc67fc86d87f8c33eccc26
Ajstros <ajstros@gmail.com>
2022-06-16 16:59:58 -0500

    Move interfaces to src, separate chips into peripherals folder with individual files

---
+++ python/src/interfaces/peripherals/SPIFifoDriven.py
@@ -0,0 +1,184 @@
+class SPIFifoDriven():
+    """Class for SPI controllers on the FPGA driven by a FIFO.
+
+    Attributes
+    ----------
+    default_data_mux : dict
+        Class attribute. Default data_mux dictionary.
+    fpga : FPGA
+        FPGA instance this controller uses to communicate.
+    endpoints : dict
+        Endpoints on the FPGA this controller uses to communicate.
+    master_config : int
+        Value of the CTRL register in the Wishbone.
+    data_mux: dict
+        Name to select value dictionary for source data MUX.
+    current_data_mux : str
+        Name of the current MUX data source.
+    """
+
+    default_data_mux = {
+        'DDR': 0,
+        'host': 1,
+        'ads8686_chA': 2,
+        'DDR_norepeat': 3,  # does not send an update if the DDR data is the same. Reduces digital switching noise on the SPI bus
+        'ad7961_ch0': 4,
+        'ad7961_ch1': 5,
+        'ad7961_ch2': 6,
+        'ad7961_ch3': 7,
+    }
+
+    def __init__(self, fpga, endpoints, master_config, data_mux=default_data_mux):
+        self.fpga = fpga
+        self.endpoints = endpoints
+        self.master_config = master_config
+        self.data_mux = data_mux
+        # Start with host to minimize unintentional commands from other sources on startup
+        self.current_data_mux = None
+        if self.set_data_mux('host') == -1:
+            # 'host' was not in data_mux dict
+            print("WARNING: no 'host' in data_mux dict")
+
+    @classmethod
+    def create_chips(cls, fpga, number_of_chips, endpoints=None, master_config=None):
+        """Instantiate a number of new chips.
+
+        The number must be an integer greater than zero. The endpoints between
+        each instance will be incremented. If the endpoints argument is left
+        as None, then we will use copies of the endpoints_from_defines
+        dictionary for the endpoints for each instance, and update that
+        original dictionary when we increment the endpoints. This way, the
+        endpoints there are ready for another instantiation if needed.
+        """
+
+        if type(number_of_chips) is not int or number_of_chips <= 0:
+            print('number_of_chips must be an integer greater than 0')
+            return False
+
+        chips = []
+        if master_config is None:
+            # Use class default for master_config
+            for i in range(number_of_chips):
+                chips.append(cls(fpga=fpga, endpoints=endpoints))
+                # Use deepcopy to keep the endpoints for different instances separate
+                endpoints = copy.deepcopy(chips[-1].endpoints)
+                Endpoint.advance_endpoints(endpoints)
+        else:
+            for i in range(number_of_chips):
+                chips.append(cls(fpga=fpga, endpoints=copy.deepcopy(
+                    endpoints), master_config=master_config))
+                # Use deepcopy to keep the endpoints for different instances separate
+                endpoints = copy.deepcopy(chips[-1].endpoints)
+                Endpoint.advance_endpoints(endpoints)
+
+        if endpoints is None:
+            # Increment shared endpoints dictionary
+            # TODO: is there a better way to do this?
+            # We need to get the shared endpoints in endpoints_from_defines to
+            # increment and we only have the endpoints given to us in the
+            # argument.
+            shared_full_eps = Endpoint.endpoints_from_defines
+            shared_chip_eps = shared_full_eps[
+                list(shared_full_eps.keys())[
+                    list(shared_full_eps.values()).index(chips[0].endpoints)
+                ]
+            ]
+            Endpoint.advance_endpoints(shared_chip_eps, number_of_chips)
+        else:
+            # Increment custom dictionary
+            Endpoint.advance_endpoints(endpoints, number_of_chips)
+
+        return chips
+
+    def filter_select(self, operation='set'):
+        """Set whether SPI data comes from filter ('set') or direct ('clear')
+
+        Direct comes from the spi_fifo_driven data.
+        """
+
+        if operation == 'set':
+            self.fpga.set_wire_bit(self.endpoints['FILTER_SEL'].address,
+                                   self.endpoints['FILTER_SEL'].bit_index_low)
+        elif operation == 'clear':
+            self.fpga.clear_wire_bit(self.endpoints['FILTER_SEL'].address,
+                                     self.endpoints['FILTER_SEL'].bit_index_low)
+        else:
+            print(f'Incorrect operation: {operation} for filter select \n')
+
+    def set_data_mux(self, source):
+        """Configure the MUX that routes data source to the SPI output.
+
+        Parameters
+        ----------
+        source : str
+            See SPIFifoDriven.data_mux dict for options and conversion.
+        """
+
+        select_val = self.data_mux.get(source)
+        if select_val is None:
+            print(f'Set data mux failed, {source} not available')
+            return -1
+
+        mask = gen_mask(range(self.endpoints['DATA_SEL'].bit_index_low,
+                              self.endpoints['DATA_SEL'].bit_index_high))
+        data = (self.data_mux[source]
+                << self.endpoints['DATA_SEL'].bit_index_low)
+        self.fpga.set_wire(self.endpoints['DATA_SEL'].address, data,
+                           mask=mask)
+        self.current_data_mux = source
+
+    def set_clk_divider(self, divide_value):
+        """Set clock divider to configure rate of SPI updates.
+
+        Tupdate = Tclk * divide_value
+        """
+        mask = gen_mask(range(self.endpoints['PERIOD_ENABLE'].bit_index_low,
+                              self.endpoints['PERIOD_ENABLE'].bit_index_high))
+
+        self.fpga.set_wire(self.endpoints['PERIOD_ENABLE'].address,
+                           divide_value << self.endpoints['PERIOD_ENABLE'].bit_index_low,
+                           mask)
+
+        # resets the SPI state machine
+        self.fpga.xem.ActivateTriggerIn(self.endpoints['REG_TRIG'].address,
+                                        self.endpoints['REG_TRIG'].bit_index_low)
+
+    def set_ctrl_reg(self, reg_value):
+        """Configures the SPI Wishbone control register over the registerBridge.
+
+        HDL default is ctrlValue = 16'h3010 (initialized in the HDL)
+        """
+
+        self.fpga.xem.WriteRegister(
+            self.endpoints['REGBRIDGE_OFFSET'].bit_index_low, reg_value)
+
+        # resets the SPI state machine -- needed since these registers are only
+        #   programmed at startup of the state machine
+        self.fpga.xem.ActivateTriggerIn(self.endpoints['REG_TRIG'].address,
+                                        self.endpoints['REG_TRIG'].bit_index_low)
+
+    def set_spi_sclk_divide(self, divide_value=0x01):
+        """Configures the SPI Wishbone clock divider register over the registerBridge.
+
+        HDL default is 8'h13 (initialized in the HDL)
+        """
+
+        sys_clk = 200  # in MHz
+        print('SCLK predicted frequency {:.2f} [MHz]'.format(
+            sys_clk/(divide_value+1)))
+        self.fpga.xem.WriteRegister(
+            self.endpoints['REGBRIDGE_OFFSET'].bit_index_low + 1, divide_value)
+
+        # resets the SPI state machine -- needed since these WishBone
+        #   registers are only programmed at startup of the state machine
+        self.fpga.xem.ActivateTriggerIn(self.endpoints['REG_TRIG'].address,
+                                        self.endpoints['REG_TRIG'].bit_index_low)
+
+    def write(self, data):
+        """Host write 24 bits of data to the chip over SPI."""
+
+        if self.current_data_mux != 'host':
+            self.set_data_mux('host')
+        self.fpga.set_wire(self.endpoints['HOST_WIRE_IN'].address, data)
+        self.fpga.xem.ActivateTriggerIn(self.endpoints['HOST_TRIG'].address,
+                                        self.endpoints['HOST_TRIG'].bit_index_low)
---
+++ python/src/interfaces/peripherals/TMF8801.py
@@ -0,0 +1,235 @@
+from I2CController import I2CController
+
+
+class TMF8801(I2CController):
+    """Class for the Time-of-flight I2C device
+
+    Subclass of the I2CController class. Attributes and methods below are
+    differences in this class from I2CController only.
+
+    Attributes
+    ----------
+    ADDRESS : int
+        7-bit device address with R/W bit space 
+    registers : dict
+        Name-Register pairs for the internal registers of the chip.
+    """
+
+    ADDRESS = 0b0100_0001 << 1
+    registers = Register.get_chip_registers('TMF8801')
+    apps = {'measure': 0xC0, 'bootloader': 0x80}
+    MULTI_BYTE_ORDER = 'LSB_1st'
+
+    def write(self, data, register_name):
+        """Write data to any register on the chip.
+            first reads to enable writing of (just) bit-fields within the register
+        """
+
+        dev_addr = self.ADDRESS
+        register = self.registers[register_name]
+        data <<= register.bit_index_low
+
+        # Mask only the bits for the specified register.
+        mask = 0
+        for bit in range(register.bit_index_high, register.bit_index_low - 1, -1):
+            mask |= 0x1 << bit
+
+        # Compare with current data to mask unchanged values
+        read_out = self.read(register_name)
+        if read_out == None:
+            print('Read for masking FAILED')
+            return False
+        new_data = (data & mask) | (read_out & ~mask)
+
+        # Turn data into a list of bytes for i2c_write method
+        list_data = int_to_list(new_data)
+        number_of_bytes = len(list_data)
+        list_data_str = ''.join(" ".join(f'0x{x:02x}' for x in list_data))
+        print(
+            f'i2c write long: 0x{dev_addr:02x}, reg addr 0x{register.address:02x}, data {list_data_str}')
+        self.i2c_write_long(
+            dev_addr, [register.address], number_of_bytes, list_data)
+
+    def read(self, register_name, number_of_bytes=None):
+        """Return data from any register on the chip."""
+
+        dev_addr = self.ADDRESS
+        register = self.registers[register_name]
+        # This is the total number of bytes to read
+        byte_number = (register.bit_index_high // 8) + 1
+        # e.g. 16-bit register = 2 bytes, i2c_read_long starts at the MSB so we read 2 bytes to get Byte 0
+        if number_of_bytes is None:
+            number_of_bytes = byte_number
+        # print(f'Read slave address: 0x{dev_addr:02x}, reg addr 0x{register.address:02x}')
+        read_back_list = self.i2c_read_long(
+            dev_addr, [register.address], number_of_bytes)
+
+        # Turn the list into an integer
+        read_back_data = 0
+        if read_back_list == None:
+            return None
+
+        if self.MULTI_BYTE_ORDER == 'MSB_1st':
+            for byte in read_back_list:
+                # print('Readback byte of 0x{:02X}'.format(byte))
+                read_back_data <<= 8
+                read_back_data |= byte
+        elif self.MULTI_BYTE_ORDER == 'LSB_1st':
+            for byte in read_back_list[::-1]:  # flip list order
+                # print('Readback byte of 0x{:02X}'.format(byte))
+                read_back_data <<= 8
+                read_back_data |= byte
+
+        # Get only the bits for the specified register from what was read back.
+        desired_bits = 0
+        for bit in range(register.bit_index_high, register.bit_index_low - 1, -1):
+            desired_bits += 0x1 << bit
+        desired_data = (read_back_data
+                        & desired_bits) >> register.bit_index_low
+
+        return desired_data
+
+    def power_down_high_impedance(self):
+        """Power down the DAC output to high impedance (default)."""
+
+        pass
+        # self.write(0b10, 'DAC_PDN')
+
+    def get_id(self):
+        """Return the device ID and rev ID as one integer."""
+
+        device_id = self.read('ID')
+        version_id = self.read('REVID')
+        return (device_id << self.registers['ID'].bit_index_low) | (version_id << self.registers['ID'].bit_index_low)
+
+    def cpu_reset(self):
+        self.write(1, 'CPU_RESET')
+
+    def cpu_ready(self):
+        return self.read('CPU_READY', 1)
+
+    def load_app(self, app='measure'):
+
+        if app == 'measure':
+            val = 0xC0
+        elif app == 'bootloader':
+            val = 0x80
+        self.write(val, 'APPREQID')
+
+        return self.read('APPID')
+
+    def read_app(self):
+
+        appid = self.read('APPID')
+        appname = list(self.apps.keys())[list(self.apps.values()).index(appid)]
+        print(f'In app {appname}')
+        return appid
+
+    def rom_fw_version(self):
+        rev_major = self.read('APPREV_MAJOR', number_of_bytes=1)
+        rev_minor = self.read('APPREV_MINOR', number_of_bytes=1)
+        patch = self.read('APPREV_PATCH', number_of_bytes=1)
+
+        print(f'major: {rev_major}, minor: {rev_minor}, patch: {patch}')
+
+        return rev_major, rev_minor, patch
+
+    def bl_command(self, cmd):
+
+        appid = self.read('APPID')
+        if appid == self.apps['bootloader']:
+            print('cannot send boatloader command when in measure app')
+            return 0
+        if cmd == 'ramremap_reset':
+            val = 0x11
+        elif cmd == 'download_init':
+            val = 0x14
+
+        self.write(val, 'CMD_DATA7')
+
+    def ram_write_status(self):
+        # TODO: Host_Driver_Comm document suggests this reads back 3 bytes
+        return self.read('CMD_DATA7', number_of_bytes=3)
+
+    def download_init(self):
+
+        dev_addr = self.ADDRESS
+        reg_addr = self.registers['CMD_DATA7'].address
+        # TODO: why 0x29 - specified by host driver comms
+        data = [0x14, 0x01, 0x29, 0xC1]
+        self.i2c_write_long(dev_addr, [reg_addr],
+                            len(data), data)
+
+    def ramremap_reset(self):
+
+        dev_addr = self.ADDRESS
+        reg_addr = self.registers['CMD_DATA7'].address
+        data = [0x11, 0x00, 0xEE]
+        self.i2c_write_long(dev_addr, [reg_addr],
+                            len(data), data)
+
+    def read_dist_peak(self):
+
+        dev_addr = self.ADDRESS
+        reg_addr = self.registers['DISTANCE_PEAK'].address
+        num_bytes = self.registers['DISTANCE_PEAK'].bit_width//8
+        return self.i2c_read_long(dev_addr, [reg_addr],
+                                  data_length=num_bytes)
+
+    def read_data(self):
+        """ For correctly updating of these registers by TMF8801, an I²C block read starting from address 0x1D
+        until 0x27 shall be done.
+        0x1D, 0x1E, 0x1F, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27"""
+
+        dev_addr = self.ADDRESS
+        reg_addr = self.registers['STATUS'].address
+
+        read_data = self.i2c_read_long(dev_addr, [reg_addr],
+                                       data_length=11)
+
+        vals = {'status': read_data[0],
+                'register_contents': read_data[1],
+                'transaction_id': read_data[2],
+                'result_num': read_data[3],
+                'result_info': read_data[4],
+                'dist_mm': read_data[5] + (read_data[6] << 8),
+                'sys_clk': read_data[7] + (read_data[8] << 8) + (read_data[9] << 16) + (read_data[10] << 24)}
+        vals['sys_clk_seconds'] = vals['sys_clk']*0.2e-6
+        return vals, read_data
+
+    def read_by_addr(self, reg_addr, num_bytes=1):
+        dev_addr = self.ADDRESS
+        return self.i2c_read_long(dev_addr, [reg_addr],
+                                  data_length=num_bytes)
+
+    def factory_calibration(self, filename=None):
+        """
+        Perform factory calibration and return 14 bytes of data
+        See AN000597 -- Factor Calibration 8.1
+
+        Calibration Environment: Device has to be in the final (correct) optical stack
+        Clear glass (no smudge on the glass)
+        No target in front of the device within 40 cm (see datasheet)
+        Dark room or low ambient light
+        """
+        print('Factory calibration: device should be in dark room with no objects within 40 cm')
+
+        self.write(0x0A, 'COMMAND')  # Command is register 0x10
+        time.sleep(2)
+        cal_done = 0
+        count = 0
+        while (cal_done != 0x0A) and (count < 20):
+            cal_done = self.read('REGISTER_CONTENTS')
+            sleep(0.1)
+            count = count + 1
+            print(f'Cal done 0x{cal_done:02x}')
+
+        cal_data = self.read_by_addr(0x20, num_bytes=14)
+        print('Calibration data')
+        for d in cal_data:
+            print(d)
+
+        with open(filename, 'w') as f:
+            f.write(','.join(cal_data))
+
+        return cal_data
