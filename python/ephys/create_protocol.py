fe806738d2be1e1b0baa354d481dd41d88b128e1
Ajstros <ajstros@gmail.com>
2022-06-28 14:03:19 -0500

    Update Protocol.preview to return ax for user to show

--- python/ephys/create_protocol.py
+++ python/ephys/create_protocol.py
@@ -3,6 +3,7 @@
 Protocol -> Sweeps -> Epochs
 """
 
+import matplotlib.pyplot as plt
 from core import Protocol
 
 protocol = Protocol.create_from_csv(filepath='protocol.csv', num_sweeps=5)
@@ -10,3 +11,5 @@ protocol.preview()
 
 protocol2 = Protocol.create_from_csv(filepath='protocol2.csv', num_sweeps=12)
 protocol2.preview()
+
+plt.show()
