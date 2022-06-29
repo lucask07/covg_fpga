f6196f32277e9cd71e13378e352e2cdf0d7d8901
Ajstros <ajstros@gmail.com>
2022-06-29 14:40:56 -0500

    Add Experiment outline

--- python/ephys/core.py
+++ python/ephys/core.py
@@ -1,15 +1,26 @@
-"""Classes for electrophysiology."""
+"""Classes for electrophysiology.
 
-from typing import List
+Abe Stroschein, ajstroschein@stthomas.edu
+"""
+
+from typing import List, Dict
 import copy
+import time
 import numpy as np
 import matplotlib.pyplot as plt
-from pyripherals.core import FPGA
+import atexit
+from pyripherals.core import FPGA, Endpoint
+from pyripherals.utils import from_voltage
 from pyripherals.peripherals.DDR3 import DDR3
+from ..instruments.power_supply import open_rigol_supply, pwr_off, config_supply
+from ..boards import Daq, Clamp
 
+# Variables
 f = FPGA()
 f.init_device()
 ddr = DDR3(fpga=f)
+
+# Classes
 
 class Epoch:
     """One division of time in a CMD signal.
@@ -222,3 +233,115 @@ class Protocol:
         sweep = Sweep(epochs=epochs)
         protocol = Protocol(sweep=sweep, num_sweeps=num_sweeps)
         return protocol
+
+
+class Experiment:
+    """Holds all objects needed for an Experiment.
+    
+    Attributes
+    ----------
+    endpoints : Dict[str, Endpoint]
+        All Endpoints used in the experiment.
+    fpga : FPGA
+        The FPGA instance connected to the Daq board.
+    daq : Daq
+        The Daq instance conneced to the Clamp boards.
+    clamps : List[Clamp]
+        List of Clamp instances available on the Daq board. These can be connected to cells.
+    gpio : Daq.GPIO
+        GPIO pins on Daq board.
+    pwr : Daq.POWER
+        Power for Daq board.
+    dc_pwr : ???
+        The power supply instances powering the Daq board.
+    """
+
+    def __init__(self):
+
+        # Initialize FPGA
+        self.endpoints = Endpoint.update_endpoints_from_defines()
+        self.fpga = FPGA()
+        self.daq = Daq(self.fpga)
+        self.clamps = [Clamp(f, dc_num=dc_num) for dc_num in range(4)]
+        self.gpio = Daq.GPIO(self.fpga)
+        self.pwr = Daq.Power(self.fpga)
+        self.pwr.all_off()  # disable all power enables
+        self.dc_pwr = []
+
+    def setup(self):
+        """Setup necessary for reading and writing data with a connection to a model cell."""
+
+        # Set up power supplies
+        pwr_setup = "3dual"
+        self.dc_pwr = open_rigol_supply(setup=pwr_setup)
+        if pwr_setup == "3dual":
+            atexit.register(pwr_off, [self.dc_pwr[0]])
+        else:
+            atexit.register(pwr_off, [self.dc_pwr])
+        config_supply(self.dc_pwr[0], self.dc_pwr[1], setup=pwr_setup, neg=15)
+
+        # Turn on power supplies
+        self.dc_pwr[0].set("out_state", "ON", configs={"chan": 1})  # turn on the 7V
+        if pwr_setup != "3dual":
+            # turn on the +/-16.5 V input
+            for ch in [1, 2]:
+                self.dc_pwr[1].set("out_state", "ON", configs={"chan": ch})
+        elif pwr_setup == "3dual":
+            # turn on the +/-16.5 V input
+            for ch in [2, 3]:
+                self.dc_pwr[0].set("out_state", "ON", configs={"chan": ch})
+
+        # Initialize the FPGA
+        f.init_device()
+        time.sleep(2)
+        f.send_trig(self.endpoints["GP"]["SYSTEM_RESET"])  # system reset
+        self.daq.ADC[0].reset_wire(1)    # Only actually one WIRE_RESET for all AD7961s
+
+        # power supply turn on via FPGA enables
+        for name in ["1V8", "5V", "3V3"]:
+            self.pwr.supply_on(name)
+            time.sleep(0.05)
+
+        # configure the SPI debug MUXs
+        self.gpio.spi_debug("dfast1")
+        self.gpio.ads_misc("sdoa")  # do not care for this experiment
+
+        # instantiate the Clamp board providing a daughter card number (from 0 to 3)
+        for dc_num in range(len(self.clamps)):
+            print(f'Clamp {dc_num} Init'.center(35, '-'))
+            self.clamps[dc_num].init_board()
+            self.clamps[dc_num].DAC.write(data=from_voltage(voltage=0.9940/1.6662,
+                            num_bits=10, voltage_range=5, with_negatives=False))
+
+        # --------  Enable fast ADCs  --------
+        for chan in [0, 1, 2, 3]:
+            self.daq.ADC[chan].power_up_adc()  # standard sampling
+        time.sleep(0.1)
+        self.daq.ADC[0].reset_wire(0)    # Only actually one WIRE_RESET for all AD7961s
+        time.sleep(1)
+
+        # TODO: are the TCA pins already configured in Clamp.configure_clamp()?
+        self.daq.TCA[0].configure_pins([0, 0])
+        self.daq.TCA[1].configure_pins([0, 0])
+
+        # fast DAC channels setup
+        for i in range(6):
+            self.daq.DAC[i].set_ctrl_reg(self.daq.DAC[i].master_config)
+            self.daq.DAC[i].set_spi_sclk_divide()
+            self.daq.DAC[i].filter_select(operation="clear")
+            self.daq.DAC[i].write(int(0))
+            self.daq.DAC[i].set_data_mux("DDR")
+            self.daq.DAC[i].change_filter_coeff(target="passthru")
+            self.daq.DAC[i].write_filter_coeffs()
+            self.daq.set_dac_gain(i, 5)  # 5V to see easier on oscilloscope
+
+    def close(self):
+        """Close opened devices from setup."""
+
+        self.pwr.all_off()
+        pwr_off(self.dc_pwr)
+
+    def record(self):
+        """Send out Sequence and record data back."""
+
+        pass
