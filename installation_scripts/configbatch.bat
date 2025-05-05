d0b6d74272f0493b647032b0896ba952da9b0527
Dat Dinh <dinh3332@stthomas.edu>
2025-05-05 16:15:32 -0500

    add a progress bar to pip install and termination right after any failed batch operation

--- installation_scripts/configbatch.bat
+++ installation_scripts/configbatch.bat
@@ -28,16 +28,19 @@ if "!PYVERONE!" geq "3" (
 echo Start installing pip from requirements.txt >> "..\log.txt"
 echo Start installing pip from requirements.txt
 :: pip install -r python\requirements.txt :: This line is to be used if we don't want to simplify the stdoutput and stderr
-python pip_custom_install.py "../python/requirements.txt" "../log.txt"
+python pip_custom_install.py "../python/requirements.txt" "../log.txt" || goto :error
 echo Finished installing pip
 echo Finished installing pip >> "..\log.txt"
 
 :: Install Registers.xlsx
 echo Start installing Registers.xlsx >> "..\log.txt"
-curl https://github.com/Ajstros/pyripherals/blob/main/python/Registers.xlsx -o ..\python\Registers.xlsx
+echo Start installing Registers.xlsx
+curl https://github.com/Ajstros/pyripherals/blob/main/python/Registers.xlsx -o ..\python\Registers.xlsx || goto :error
 echo Finished installing Registers.xlsx >> "..\log.txt"
+echo Finished installing Registers.xlsx
 
 echo Writing ~\.pyripherals\config.yml >> "..\log.txt"
+echo Writing ~\.pyripherals\config.yml
 :: Make .pyripherals directories inside the user's home directory
 :: The command `mkdir` will not give an error if the directories already exist
 mkdir "%USERPROFILE%\.pyripherals" >nul 2>&1
@@ -46,7 +49,7 @@ set PYRI="%USERPROFILE%\.pyripherals"
 for /f "delims=" %%a in ('cd .. ^& cd') do set ROOT_DIR=%%a
 
 :: Modify the config.yaml
-type nul > %PYRI%\config.yaml
+type nul > %PYRI%\config.yaml || goto :error
 echo endpoint_max_width: 32 > %PYRI%\config.yaml
 echo ep_defines_path: %ROOT_DIR%\fpga_XEM7310\fpga_XEM7310.srcs\sources_1\ep_defines.v >> %PYRI%\config.yaml
 echo fpga_bitfile_path: %ROOT_DIR%\fpga_XEM7310\fpga_XEM7310.runs\impl_1\top_level_module.bit >> %PYRI%\config.yaml
@@ -55,26 +58,37 @@ echo registers_path: %ROOT_DIR%\python\Registers.xlsx >> %PYRI%\config.yaml
 :: Find FrontPanelUSB directory (equivalent to "find / -name FrontPanelUSB") - only take the first line of the standard output
 set OPAL_FOUND=false
 echo Finding the path of the Opal Kelly API >> "..\log.txt"
+echo Finding the path of the Opal Kelly API
 for /f "delims=" %%b in ('dir /s /b C:\FrontPanelUSB 2^>nul') do (
 	if "!OPAL_FOUND!"=="false" (
 		set WHERE_OPAL=%%b
 		set OPAL_FOUND=true
 	)
-)
+) || goto :error
 
 echo Done finding the path to Opal Kelly - The path is: %WHERE_OPAL% >> "..\log.txt"
+echo Done finding the path to Opal Kelly - The path is: %WHERE_OPAL%
 
 echo frontpanel_path: %WHERE_OPAL% >> %PYRI%\config.yaml
 echo Finished writing .pyripherals\config.yml >> "..\log.txt"
+echo Finished writing .pyripherals\config.yml
 
 :: Create config_yaml for instrbuilder
 echo Initiate the config_yaml for the instrbuilder module >> "..\log.txt"
-python create_yaml_instrbuilder.py
+echo Initiate the config_yaml for the instrbuilder module
+python create_yaml_instrbuilder.py || goto :error
 echo Done initiating instrbuilder's config_yaml >> "..\log.txt"
+echo Done initiating instrbuilder's config_yaml
 
 ::Inform users that configuration has finished
 echo Configuration finished!
 echo Configuration finished! >> "..\log.txt"
+
+:error
+if %ERRORLEVEL% NEQ 0 (
+	echo This installation script has been terminated due to a failed operation 1>&2
+)
+exit /b %ERRORLEVEL%
 
 endlocal
 :: Done
--- installation_scripts/pip_custom_install.py
+++ installation_scripts/pip_custom_install.py
@@ -11,16 +11,33 @@ LOG_NAME = sys.argv[2]
 REQUIREMENTS = sys.argv[1]
 COMMENT_MARKER = "#"
 
-def install_packages(package):
-    """
-    This handles each package manually and so we have a 
-    total control of what the stdout and stderr messages should be.
-    """
-    result = subprocess.run(["pip", "install", package], capture_output = True, text = True)
-    if result.returncode == 0:
-        write_to_log(f"Package {package} installed successfully!\n")
-    else:
-        write_to_log(f"Failed to install {package} package!\n")
+class PackageProgress:
+    def __init__(self, total_packages):
+        self.progress_bar = str()
+        self.installed = 0
+        self.total_packages = total_packages
+        self.SUCCESS_ASSOCIATED = "█"
+        self.YET_ASSOCIATED = "-"
+    
+    def print_progress(self):
+        percent = 100 * (self.installed / float(self.total_packages))
+        self.progress_bar = self.SUCCESS_ASSOCIATED * int(percent/1.5) + self.YET_ASSOCIATED * (int(100/1.5) - int(percent/1.5))
+        print(f"\r[{self.progress_bar}] {percent:.2f}%", end = "")
+    
+    def install_packages(self, package):
+        """
+        This handles each package manually and so we have a 
+        total control of what the stdout and stderr messages should be.
+        """
+        result = subprocess.run(["pip", "install", package], capture_output = True, text = True)
+        if result.returncode == 0:
+            self.installed += 1
+            write_to_log(f"Package {package} installed successfully!\n")
+            self.print_progress()
+        else:
+            write_to_log(f"Failed to install {package} package!\n")
+            self.print_progress()
+            sys.exit(f"\nFailed to install {package} package!\n")
 
 def write_to_log(message):
     """
@@ -32,6 +49,7 @@ def write_to_log(message):
         slug.write(message)
 
 try:
+    packages = []
     with open(REQUIREMENTS, "r") as f:
         for pack in f:
             if pack.startswith(COMMENT_MARKER):
@@ -39,7 +57,11 @@ try:
             comment_marker_index = pack.find(COMMENT_MARKER)
             if comment_marker_index != -1:
                 pack = pack[:comment_marker_index]
-            install_packages(pack.strip())
+            packages.append(pack.rstrip())
+    progress_obj = PackageProgress(len(packages))
+    [progress_obj.install_packages(package) for package in packages]
+    print()
 except IOError:
     write_to_log("Installing scripts - requirements.txt - not found")
     print("Installing scripts - requirements.txt - not found")
+    sys.exit(1)
